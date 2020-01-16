0d70483c162af895e7664360c130dbc141352a0e
papillon078 <ldc.morin@gmail.com>
2020-01-16 12:24:28 +0100

    exo 6 partie 1 apres correction

---
+++ exo6.sql
@@ -0,0 +1,3 @@
+-----------------------PARTIE 1 EXERCICE 6 CORRECTION------------------------------------
+-- Supprimer la base de données frameworks si elle existe
+DROP DATABASE IF EXISTS `frameworks`;
